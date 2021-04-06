8b4705abdaf2d6f584557343565a8d4c5010bf40
ianrigsbee54 <irrigsbee@miners.utep.edu>
2021-04-06 00:20:19 -0600

    vastly simplified already complicated method for receiving a message by fully redoing it

--- tcpLab/frameSock.py
+++ tcpLab/frameSock.py
@@ -7,33 +7,22 @@ def frameSend(sock, fileName, fileData):
     while len(msg):
         sentMsg = sock.send(msg)#continuously send file contents
         msg = msg[sentMsg:]#cut message length 
+
+
     
 rbuff = b""
 def frameRecv(sock):
     global rbuff
-    state = 1
-    msgLength = -1
+    recvMessage = sock.recv(100)
+    framedMessage = recvMessage.split(b':')
+    fileName = framedMessage[1].decode()
+    msgLength = int(framedMessage[0])
+    rbuff += framedMessage[2]
     while True:
-        if state == 1:
-            #use regex to split string into three parts for our variables
-            match = re.match(b'([^:]+):(.*):(.*)', rbuff, re.DOTALL | re.MULTILINE)
-            if match:
-                strLength, fileName, rbuff = match.groups()#set variables from regex groups
-                try:
-                    msgLength = int(strLength)
-                except:
-                    if len(rbuff):
-                        print("message incorrectly formatted")
-                        return None, None
-                state = 2
-        if state == 2: #start sending file contents and file name
-            if len(rbuff) >= msgLength: #once we have all file content. Send it
-                fileData = rbuff[0:msgLength]
-                rbuff = rbuff[msgLength:]
-                return fileName, fileData
-        recMessage = sock.recv(1024) #receive data and put it in buffer 
-        rbuff += recMessage
-        if len(recMessage) == 0:#if nothing received and buffer still has content. Then quit
-            if len(rbuff) != 0:
-                print("incomplete message")
-            return None, None
+        if len(rbuff) >= msgLength:
+            fileData = rbuff[0:msgLength]
+            rbuff = rbuff[msgLength:]
+            return fileName, fileData
+        recvMessage = sock.recv(100)
+        rbuff+=recvMessage
+
